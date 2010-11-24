f43ca7fd6b2197c5d15e7064a09c6b58f60d1611
unknown <Michael_2@.(none)>
2010-11-25 00:06:57 +0200

    css changes. js file and backgrounds addition for mousemove showcase (continued)

---
+++ quasar/template/mouseshowcase.js
@@ -0,0 +1,26 @@
+//Author:Michael Michaelidis
+//Year: 2010
+//Username: Zifnab
+
+var mousex = 0
+
+var IE = document.all?true:false
+if (!IE) 
+	document.captureEvents(Event.MOUSEMOVE)
+document.onmousemove = getmousepos;
+
+
+function getmousepos(e) {
+  if (IE) {
+    mousex = event.clientX + document.body.scrollLeft
+  } else { 
+    mousex = e.pageX
+  }  
+  if (mousex < 0){mousex = 0}
+  var layer1 = document.getElementById("custom-header2");
+  var layer2 = document.getElementById("custom-header3");
+  var screenwidth=1280;
+  layer1.style.backgroundPosition=mousex/screenwidth*210+"% 100%";
+  layer2.style.backgroundPosition=100-mousex/screenwidth*90+"% 100%";
+  return true
+}
